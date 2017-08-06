525e992e500664ae517654d2ab59c6353a38231c
Rohan Malik <malikmalik29@outlook.com>
2017-08-06 18:51:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohanmalik/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rohanmalik.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView textView = (TextView)findViewById(R.id.text);
+        textView.setText("Hello World");
+    }
+}
